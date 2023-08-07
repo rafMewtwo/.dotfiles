d924f2f1fabc99009fad804e1a97b4d1291b49ce
Ito <italoaa99@gmail.com>
2023-08-07 03:49:45 -0300

    making nvim-tree open by default

--- lvim/lua/user/options.lua
+++ lvim/lua/user/options.lua
@@ -1,7 +1,9 @@
 lvim.log.level = "warn"
 lvim.transparent_window = true
+
 lvim.builtin.fancy_searching.hl_group = "Normal"
 lvim.builtin.telescope.defaults.file_ignore_patterns = { ".git", "node_modules" }
 
 vim.opt.shiftwidth = 2 --the number of spaces inserted for each  indentation
 vim.opt.tabstop = 2 --insert 2 spaces for a tab
+
